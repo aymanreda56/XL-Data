87850dbfb90af127a4af2da8e8aca9b545b634d0
Marim1611 <marimnaser813@gmail.com>
2023-05-10 18:26:09 +0300

    silhouette method

--- DataAnalysis/Clustering/utils.py
+++ DataAnalysis/Clustering/utils.py
@@ -32,4 +32,28 @@ def elbow_method(data, max_clusters=10):
     return sse.index(min(sse)) + 1
 
 
-
+def silhouette_method(data, max_clusters=10):
+    '''
+    function to return the optimal number of clusters according
+     to the silhouette so that silhouette score is maximized
+    '''
+    # create empty list to store the silhouette scores
+    silhouette_scores = []
+    # create a list of number of clusters
+    clusters = range(2, max_clusters)
+    # loop through the clusters
+    for k in clusters:
+        # create a k-means model with k clusters
+        kmeans = KMeans(n_clusters=k)
+        # fit the model to the data
+        kmeans.fit(data)
+        # append the silhouette score to the list
+        silhouette_scores.append(silhouette_score(data, kmeans.labels_))
+    # plot the silhouette curve
+    plt.plot(clusters, silhouette_scores)
+    plt.xlabel("Number of clusters")
+    plt.ylabel("Silhouette score")
+    plt.title("Silhouette curve")
+    plt.show()
+    # return the optimal number of clusters
+    return silhouette_scores.index(max(silhouette_scores)) + 2
