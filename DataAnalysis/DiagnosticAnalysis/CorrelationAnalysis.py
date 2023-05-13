eb67f36f3f63570d72a372d10662bbf23b88278d
Marim1611 <marimnaser813@gmail.com>
2023-05-13 23:39:27 +0300

    ANOVA test

--- DataAnalysis/DiagnosticAnalysis/CorrelationAnalysis.py
+++ DataAnalysis/DiagnosticAnalysis/CorrelationAnalysis.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import scipy.stats as ss
+from scipy.stats import f_oneway
 
 import sys; sys.path.append("../../")
 from DataPreprocessing.DataPreprocessing import encode_categ_features
@@ -110,3 +111,22 @@ def mix_correlation_matrix(df):
                 annot=True, cmap='coolwarm')
     plt.title('Mixed Correlation Matrix')
     plt.show()
+
+
+def ANOVA_test(df, cat_feature, num_feature, cats=[]):
+    '''
+    This function computes the ANOVA test between a categorical feature and a numerical feature
+    can take df, categorical feature and numerical feature as input
+    and optional list of  specific categories of the categorical feature
+    '''
+    if len(cats)==0:
+        cats = df[cat_feature].unique()
+    # Compute ANOVA test
+    groups = []
+    for category in cats:
+        groups.append(df[df[cat_feature]==category] [num_feature])
+    f_value, p_value = f_oneway(*groups)
+
+    # Print the F-value and p-value
+    print("P-value: ", p_value)
+    return p_value
