9be37b96be78af83a3973c56cab1e968598fc148
Marim1611 <marimnaser813@gmail.com>
2023-05-13 23:40:15 +0300

    subplots

---
+++ DataAnalysis/DiagnosticAnalysis/utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+def sub_blot(df, cat_col, grouby_col, col, title="",plot_kind='bar'):
+    fig, axes = plt.subplots(3, 4, figsize=(20, 10))
+    for i, (genre, ax) in enumerate(zip(df[cat_col].unique(), axes.flatten())):
+        df_genre = df[df[cat_col] == genre]
+        publisher_sales = df_genre.groupby(grouby_col)[col].mean().sort_values(ascending=False).head(10)
+        publisher_sales.plot(kind=plot_kind, ax=ax, title=genre)
+    plt.tight_layout()
+    plt.show()
