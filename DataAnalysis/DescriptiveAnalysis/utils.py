5ef1ee162991487233f26a2f0dc38517f94838d5
Mariem Muhammed <76264155+mariemzayn18@users.noreply.github.com>
2023-05-15 12:02:41 +0300

    Update plot palettes

--- DataAnalysis/DescriptiveAnalysis/utils.py
+++ DataAnalysis/DescriptiveAnalysis/utils.py
@@ -1,18 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
-def plots(x,y,xlabel,ylabel,title, style='dark_background',y_limit=False, line_plot=False, stacked=False, y2=None, label2=None):
+def plots(x, y, xlabel, ylabel, title, style='dark_background', y_limit=False, line_plot=False, stacked=False, y2=None, label2=None):
     plt.figure(figsize=(20, 10))
     plt.style.use(style)
     plt.grid(False)
 
+    # Define the color palette
+    colors = sns.color_palette('Blues', len(x))[::-1]
+
     if line_plot:
-        plt.plot(x,y)
+        plt.plot(x, y, color=colors[0])
     else:
-        plt.bar(x,y)
+        plt.bar(x, y, color=colors)
 
         if stacked:
-            plt.bar(x, y2, bottom=y, label=label2)
+            plt.bar(x, y2, bottom=y, label=label2, color=colors[1])
             plt.legend()
 
     plt.xlabel(xlabel)
@@ -20,16 +24,23 @@ def plots(x,y,xlabel,ylabel,title, style='dark_background',y_limit=False, line_p
     plt.title(title)
     plt.xticks(rotation=90)
 
-    if y_limit:
-        plt.ylim(0, 5)
-    
+    # if y_limit:
+    #     plt.ylim(0, 5)
+
+    # always start y axis at 0
+    plt.ylim(ymin=0)
+
     plt.show()
 
-
-def pie_plots(x,labels,title, style='dark_background'):
+def pie_plots(x, labels, title, style='dark_background'):
     plt.rcParams['figure.figsize'] = [10, 10]
     plt.style.use(style)
-    plt.pie(x, labels=labels, autopct='%1.1f%%', labeldistance=1.1, textprops={'fontsize': 14})
+
+    # Define the color palette
+    colors = sns.color_palette('Blues', len(x))[::-1]
+
+    plt.pie(x, labels=[f"{label}: {value:.1f}%" for label, value in zip(labels, x)],
+            autopct='', labeldistance=1.1, textprops={'fontsize': 14}, colors=colors)
     plt.title(title, fontsize=16)
     plt.show()
 
