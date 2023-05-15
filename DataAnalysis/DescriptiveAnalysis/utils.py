7c1868df87706174c7b286379c92aa8b71383193
Mariem Muhammed <76264155+mariemzayn18@users.noreply.github.com>
2023-05-15 13:46:43 +0300

    Edited some visualizations

--- DataAnalysis/DescriptiveAnalysis/utils.py
+++ DataAnalysis/DescriptiveAnalysis/utils.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+from matplotlib.lines import Line2D
 
-def plots(x, y, xlabel, ylabel, title, style='dark_background', y_limit=False, line_plot=False, stacked=False, y2=None, label2=None):
+def plots(x, y, xlabel, ylabel, title, style='dark_background', y_limit=False,\
+           line_plot=False, stacked=False, y2=None, label2=None,horizontal=False):
     plt.figure(figsize=(20, 10))
     plt.style.use(style)
     plt.grid(False)
@@ -11,49 +13,66 @@ def plots(x, y, xlabel, ylabel, title, style='dark_background', y_limit=False, l
     colors = sns.color_palette('Blues', len(x))[::-1]
 
     if line_plot:
-        plt.plot(x, y, color=colors[0])
+        plt.plot(x, y, color=colors)
+
+    elif stacked:
+        plt.bar(x, y, color=colors[len(colors) // 2])
+        plt.bar(x, y2, bottom=y, label=label2, color=colors[-1])
+        plt.legend()
+
+    elif horizontal:
+        colors = colors[::-1]
+        plt.barh(x, y, color=colors)
+        plt.xlabel(ylabel)
+        plt.ylabel(xlabel)
+
     else:
         plt.bar(x, y, color=colors)
 
-        if stacked:
-            plt.bar(x, y2, bottom=y, label=label2, color=colors[1])
-            plt.legend()
-
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+    if not horizontal:
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
     plt.title(title)
     plt.xticks(rotation=90)
 
-    # if y_limit:
-    #     plt.ylim(0, 5)
-
-    # always start y axis at 0
-    plt.ylim(ymin=0)
+    if y_limit:
+        plt.ylim(ymin=0)
 
     plt.show()
+
 
 def pie_plots(x, labels, title, style='dark_background'):
     plt.rcParams['figure.figsize'] = [10, 10]
     plt.style.use(style)
 
+    # Normalize the values in x to add up to 100
+    total = sum(x)
+    x_perc = [val/total*100 for val in x]
+
     # Define the color palette
     colors = sns.color_palette('Blues', len(x))[::-1]
 
-    plt.pie(x, labels=[f"{label}: {value:.1f}%" for label, value in zip(labels, x)],
+    plt.pie(x_perc, labels=[f"{label}: {value:.1f}%" for label, value in zip(labels, x_perc)],
             autopct='', labeldistance=1.1, textprops={'fontsize': 14}, colors=colors)
     plt.title(title, fontsize=16)
     plt.show()
 
 
-def scatter_plot_colorbar(x,y,hue,xlabel,ylabel,color_label,title, style='dark_background'):
+def scatter_plot_legend(x, y, hue, xlabel, ylabel, color_label, title, style='dark_background'):
 
     # Create a dictionary that maps each unique content to a unique color
     unique_content = list(set(hue))
     color_map = dict(zip(unique_content, np.linspace(0, 1, len(unique_content))))
 
-    # Map the content  to a sequence of numbers based on the color map
+    # Map the content to a sequence of numbers based on the color map
     colors = [color_map[content] for content in hue]
 
+    # Create a custom legend using the color map
+    legend_elements = [Line2D([0], [0], marker='o', color='w', label=key,
+                              markerfacecolor=plt.cm.rainbow(value),
+                              markersize=10) for key, value in color_map.items()]
+    
+    plt.figure(figsize=(20, 10))
     plt.style.use(style)
     plt.grid(False)
     plt.scatter(x, y, c=colors, cmap='rainbow')
@@ -61,10 +80,7 @@ def scatter_plot_colorbar(x,y,hue,xlabel,ylabel,color_label,title, style='dark_b
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
 
-    cb = plt.colorbar()
-    cb.set_ticks(list(color_map.values()))
-    cb.set_ticklabels(list(color_map.keys()))
-    cb.set_label(color_label)
+    plt.legend(handles=legend_elements, loc='best')
 
     plt.title(title)
     plt.show()
